f975e419de8f6fb595274a54c00779d9973c6fcb
mdengler10 <matthew.dengler@hexawise.com>
2018-12-30 20:34:59 -0500

    Added 343 and 352 formations

--- src/FormationFactory.java
+++ src/FormationFactory.java
@@ -294,3 +294,142 @@ class F_3142 implements Formation {
     }
 
 }
+
+class F_343 implements Formation {
+
+    private Graph graph;
+    private ArrayList<Position> positions;
+
+    public F_343() {
+
+        Position gk = new Position("GK");
+        Position lcb = new Position("CB", "LCB");
+        Position rcb = new Position("CB", "RCB");
+        Position ccb = new Position("CB", "CCB");
+        Position lcm = new Position("CM", "LCM");
+        Position rcm = new Position("CM", "RCM");
+        Position rm = new Position("RM");
+        Position lm = new Position("LM");
+        Position rw = new Position("RW");
+        Position lw = new Position("LW");
+        Position st = new Position("ST");
+
+        positions = new ArrayList<>();
+        positions.add(gk);
+        positions.add(lcb);
+        positions.add(rcb);
+        positions.add(ccb);
+        positions.add(lcm);
+        positions.add(rcm);
+        positions.add(rm);
+        positions.add(lm);
+        positions.add(rw);
+        positions.add(lw);
+        positions.add(st);
+
+        this.positions = positions;
+
+        graph = new Graph(this.positions);
+        graph.addEdge(rcb, rm);
+        graph.addEdge(rcb, ccb);
+        graph.addEdge(rcb, gk);
+        graph.addEdge(lcb, lm);
+        graph.addEdge(lcb, ccb);
+        graph.addEdge(lcb, gk);
+        graph.addEdge(ccb, gk);
+        graph.addEdge(ccb, lcm);
+        graph.addEdge(ccb, rcm);
+        graph.addEdge(lm, lcm);
+        graph.addEdge(lm, lw);
+        graph.addEdge(rm, rcm);
+        graph.addEdge(rm, rw);
+        graph.addEdge(lcm, rcm);
+        graph.addEdge(lcm, st);
+        graph.addEdge(rcm, st);
+        graph.addEdge(rw, st);
+        graph.addEdge(lw, st);
+
+        this.graph = graph;
+
+    }
+
+    public ArrayList<Position> getPositions() {
+        return this.positions;
+    }
+
+    public Graph getGraph() {
+        return this.graph;
+    }
+
+}
+
+class F_352 implements Formation {
+
+    private Graph graph;
+    private ArrayList<Position> positions;
+
+    public F_352() {
+
+        Position gk = new Position("GK");
+        Position lcb = new Position("CB", "LCB");
+        Position rcb = new Position("CB", "RCB");
+        Position ccb = new Position("CB", "CCB");
+        Position lcdm = new Position("CDM", "LCDM");
+        Position rcdm = new Position("CDM", "RCDM");
+        Position rm = new Position("RM");
+        Position lm = new Position("LM");
+        Position cam = new Position("CAM");
+        Position lst = new Position("ST", "LST");
+        Position rst = new Position("ST", "RST");
+
+        positions = new ArrayList<>();
+        positions.add(gk);
+        positions.add(lcb);
+        positions.add(rcb);
+        positions.add(ccb);
+        positions.add(lcdm);
+        positions.add(rcdm);
+        positions.add(rm);
+        positions.add(lm);
+        positions.add(cam);
+        positions.add(lst);
+        positions.add(rst);
+
+        this.positions = positions;
+
+        graph = new Graph(this.positions);
+        graph.addEdge(rcb, rm);
+        graph.addEdge(rcb, ccb);
+        graph.addEdge(rcb, gk);
+        graph.addEdge(rcb, rcdm);
+        graph.addEdge(lcb, lm);
+        graph.addEdge(lcb, ccb);
+        graph.addEdge(lcb, gk);
+        graph.addEdge(lcb, lcdm);
+        graph.addEdge(ccb, gk);
+        graph.addEdge(ccb, lcdm);
+        graph.addEdge(ccb, rcdm);
+        graph.addEdge(lm, lcdm);
+        graph.addEdge(lm, lst);
+        graph.addEdge(rm, rcdm);
+        graph.addEdge(rm, rst);
+        graph.addEdge(lcdm, rcdm);
+        graph.addEdge(lcdm, cam);
+        graph.addEdge(rcdm, cam);
+        graph.addEdge(cam, lst);
+        graph.addEdge(cam, rst);
+        graph.addEdge(lst, rst);
+
+        this.graph = graph;
+
+    }
+
+    public ArrayList<Position> getPositions() {
+        return this.positions;
+    }
+
+    public Graph getGraph() {
+        return this.graph;
+    }
+
+}
